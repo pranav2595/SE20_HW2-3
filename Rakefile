76c8ba3f6d85e5d3b3efb3c65767d32d5383ec3f
maharshi-gg <maharshig.parekh2014@gmail.com>
2020-08-24 22:36:33 -0400

    Added Ruby unit test

---
+++ Rakefile
@@ -0,0 +1,7 @@
+desc 'Run Ruby Unit Test'
+
+task :test do
+    ruby "test/test.rb"
+end
+
+task :default => :test
---
+++ test/test.rb
@@ -0,0 +1,8 @@
+require "test/unit"
+require_relative '../code/ruby/game'
+
+class GameTest < Test::Unit::TestCase
+	def sampleTest
+		assert(2+3 == 5)
+	end  
+end
